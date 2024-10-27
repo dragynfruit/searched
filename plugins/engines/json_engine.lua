1ad6bdebb2813d277dadcf4160632d295f20f2d2
drakeerv <drakeerv@outlook.com>
2024-10-27 14:36:23 -0400

    Fix wiby

--- plugins/engines/json_engine.lua
+++ plugins/engines/json_engine.lua
@@ -7,21 +7,11 @@ add_engine("json_engine", function(url, query)
     local data = parse_json(res)
 
     local results = {}
-	-- if data[2] ~= nil then
-	-- 	for i, _ in ipairs(data[2]) do
-	-- 		if data[3] ~= nil then
-	-- 			results[i] = {
-	-- 				title = data[2][i],
-	-- 				url = data[3][i],
-	-- 			}
-	-- 		end
-	-- 	end
-	-- end
     for i, _ in ipairs(data) do
         results[i] = {
-            title = data[i].title,
-            url = data[i].url,
-            snippet = data[i].snippet
+            title = data[i].Title,
+            url = data[i].URL,
+            snippet = data[i].Snippet
         }
     end
 
--- plugins/scrapers/wiby.lua
+++ plugins/scrapers/wiby.lua
@@ -3,6 +3,5 @@
 -- -- (c) 2024 Dragynfruit
 
 add_search_provider('wiby', 'sear', function (query)
-    print('Searching Wiby for ' .. query.query)
-    use_engine('json_engine')('https://wiby.me/api/search?q=%s&page=%d', query)
+    return use_engine('json_engine')('https://wiby.me/json?q=%s&page=%d', query)
 end)
