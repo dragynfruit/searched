c40a6da74b6160216c183a664576f26b41df5aff
drakeerv <drakeerv@outlook.com>
2024-10-29 16:27:44 -0400

    add crowdview

--- plugins/engines/json_engine.lua
+++ plugins/engines/json_engine.lua
@@ -2,24 +2,39 @@
 -- -- Licensed MIT.
 -- -- (c) 2024 Dragynfruit
 
+local function dump(o)
+	if type(o) == 'table' then
+	   local s = '{ '
+	   for k,v in pairs(o) do
+		  if type(k) ~= 'number' then k = '"'..k..'"' end
+		  s = s .. '['..k..'] = ' .. dump(v) .. ','
+	   end
+	   return s .. '} '
+	else
+	   return tostring(o)
+	end
+end
+
 add_engine("json_engine", function(client, _, url, opts)
-	print(opts['capitalize_json'])
 	if url ~= nil then
 		local res = client:get(url, {})
 		local data = parse_json(res)
 
+		if opts.results_key then
+			data = data[opts.results_key]
+		end
+
 		local results = {}
 		for i, _ in ipairs(data) do
 			results[i] = {
-				title = data[i].Title,
-				url = data[i].URL,
-				snippet = data[i].Snippet,
+				title = data[i][opts.title_key],
+				url = data[i][opts.url_key],
+				snippet = data[i][opts.snippet_key],
 			}
 		end
 
 		return results
 	else
-		print("wtf")
 		return {}
 	end
 end)
