d9fbaa946419e4fa10a9c9d3149a35861867af30
frc4533-lincoln <132951735+frc4533-lincoln@users.noreply.github.com>
2024-10-30 12:27:17 -0400

    Ran stylua

--- plugins/engines/json_engine.lua
+++ plugins/engines/json_engine.lua
@@ -4,7 +4,7 @@
 
 local function get_key(data, key)
 	local keys = {}
-	for k in string.gmatch(key, "([^/]+)") do
+	for k in string.gmatch(key, '([^/]+)') do
 		table.insert(keys, k)
 	end
 
@@ -19,45 +19,38 @@ local function get_key(data, key)
 	return value
 end
 
-add_engine(
-	"json_engine",
-	function(client, query, opts)
-		local url =
-			Url.from_template(
-			tostring(opts["url"]),
-			{
-				query = query.query,
-				page = tostring(query.page)
-			}
-		):string()
+add_engine('json_engine', function(client, query, opts)
+	local url = Url.from_template(tostring(opts['url']), {
+		query = query.query,
+		page = tostring(query.page),
+	}):string()
 
-		local res = client:get(url, {})
-		local data = parse_json(res)
+	local res = client:get(url, {})
+	local data = parse_json(res)
 
-		if opts.results_key then
-			data = get_key(data, opts.results_key)
-		end
+	if opts.results_key then
+		data = get_key(data, opts.results_key)
+	end
 
-		local results = {}
-		for i = 1, #data do
-			if data[i] ~= nil then
-				local result = data[i]
-				local result_url = get_key(result, opts.url_key)
+	local results = {}
+	for i = 1, #data do
+		if data[i] ~= nil then
+			local result = data[i]
+			local result_url = get_key(result, opts.url_key)
 
-				if result_url ~= nil then
-					if opts.url_prefix then
-						result_url = opts.url_prefix .. result_url
-					end
-
-					results[i] = {
-						title = get_key(result, opts.title_key),
-						url = result_url,
-						snippet = get_key(result, opts.snippet_key)
-					}
+			if result_url ~= nil then
+				if opts.url_prefix then
+					result_url = opts.url_prefix .. result_url
 				end
+
+				results[i] = {
+					title = get_key(result, opts.title_key),
+					url = result_url,
+					snippet = get_key(result, opts.snippet_key),
+				}
 			end
 		end
-
-		return results
 	end
-)
+
+	return results
+end)
