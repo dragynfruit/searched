3a92bb3ad935310d31c5c218e046b3b318e5d5f4
drakeerv <drakeerv@outlook.com>
2024-10-31 10:10:10 -0400

    fix 3

--- plugins/engines/mediawiki.lua
+++ plugins/engines/mediawiki.lua
@@ -1,36 +1,24 @@
--- -- Qwant engine for Searched
+-- -- Wikipedia scraper for Searched
 -- -- Licensed MIT.
 -- -- (c) 2024 Dragynfruit
 
-add_engine('json', function(client, query, opts)
+add_engine('mediawiki', function(client, query, opts)
 	local url = Url.from_template(tostring(opts.url), {
 		query = query.query,
-		page = tostring(query.page),
 	}):string()
 
 	local res = client:get(url, {})
 	local data = parse_json(res)
 
-	if opts.results_key then
-		data = get_key(data, opts.results_key)
-	end
-
 	local results = {}
-	for i = 1, #data do
-		if data[i] ~= nil then
-			local result = data[i]
-			local result_url = get_key(result, opts.url_key)
-
-			if result_url ~= nil then
-				if opts.url_prefix then
-					result_url = opts.url_prefix .. result_url
-				end
-
+	if data[2] ~= nil then
+		for i, _ in ipairs(data[2]) do
+			if data[4] ~= nil and data[4][i] ~= nil then
 				results[i] = {
-					title = get_key(result, opts.title_key),
-					url = result_url,
+					title = data[2][i],
+					url = data[4][i],
 					general = {
-						snippet = get_key(result, opts.snippet_key),
+						snippet = data[3][i],
 					},
 				}
 			end
